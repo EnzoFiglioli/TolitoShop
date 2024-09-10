0059b7dcbe4e90a94f548e1da2b7c369ec08bf88
Enzo Figlioli <enzofiglioli.p@gmail.com>
2024-09-10 18:36:31 -0300

    Arregle toltio server

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
   server:{
     proxy:{
       '/api':{
-        target: 'https://tolito-serverr-enzofigliolis-projects.vercel.app/', // URL de tu servidor backend
+        target: 'https://tolito-serverr-enzofigliolis-projects.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
